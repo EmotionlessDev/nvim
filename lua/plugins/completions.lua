311c66673999ce5b8d00def43f6170e8309bb5be
EmotionlessDev <emotionlesscode@gmail.com>
2024-04-09 00:17:31 +1000

    fix issues

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -50,41 +50,46 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = {
-					["<leader><Tab>"] = cmp.mapping({
-						c = function()
-							if cmp.visible() then
-								cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-							else
-								cmp.complete()
-							end
-						end,
-						i = function(fallback)
-							if cmp.visible() then
-								cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-							elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-								vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), "m", true)
-							else
-								fallback()
-							end
-						end,
-						s = function(fallback)
-							if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-								vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), "m", true)
-							else
-								fallback()
-							end
-						end,
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
 					}),
-					["<CR>"] = cmp.mapping({
-						i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
-						c = function(fallback)
-							if cmp.visible() then
-								cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-							else
-								fallback()
-							end
-						end,
-					}),
+					-- ["<leader><Tab>"] = cmp.mapping({
+					-- 	c = function()
+					-- 		if cmp.visible() then
+					-- 			cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+					-- 		else
+					-- 			cmp.complete()
+					-- 		end
+					-- 	end,
+					-- 	i = function(fallback)
+					-- 		if cmp.visible() then
+					-- 			cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+					-- 		elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
+					-- 			vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), "m", true)
+					-- 		else
+					-- 			fallback()
+					-- 		end
+					-- 	end,
+					-- 	s = function(fallback)
+					-- 		if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
+					-- 			vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), "m", true)
+					-- 		else
+					-- 			fallback()
+					-- 		end
+					-- 	end,
+					-- }),
+					-- ["<CR>"] = cmp.mapping({
+					-- 	i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+					-- 	c = function(fallback)
+					-- 		if cmp.visible() then
+					-- 			cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+					-- 		else
+					-- 			fallback()
+					-- 		end
+					-- 	end,
+					-- }),
 					-- ... Your other configuration ...
 				},
 				sources = cmp.config.sources({
